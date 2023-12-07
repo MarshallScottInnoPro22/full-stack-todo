8026a976783b844f7c2adad82c349629453acf75
MarshallScottInnoPro22 <marshallscottinnopro22@gmail.com>
2023-12-07 15:35:27 -0500

    Created middleware functions that prints request data (morgan) and reconciles files that run on different ports (cors)

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const cors = require("cors");
 dotenv.config();
 
 const app = express();
+
+app.use(cors());
+app.use(morgan("tiny"));
 
 app.get("/todos", (req, res) => {
     res.send("Hello World!");
