7e300961e9fb6bd99e36ed43af797c5bd00e6514
MarshallScottInnoPro22 <marshallscottinnopro22@gmail.com>
2023-12-07 15:45:33 -0500

    Created a login endpoint

--- server/src/index.js
+++ server/src/index.js
@@ -2,15 +2,16 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const cors = require("cors");
+
+const router = require("./router")
+
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(morgan("tiny"));
+app.use(router);
 
-app.get("/todos", (req, res) => {
-    res.send("Hello World!");
-})
 
 app.listen(8080);
