711faa5ddb2e96d35396861eec7dc866e0593ddc
MarshallScottInnoPro22 <marshallscottinnopro22@gmail.com>
2023-12-07 16:17:43 -0500

    Setup password conditionals using json web tokens

--- server/src/routes/loginRoute.js
+++ server/src/routes/loginRoute.js
@@ -1,3 +1,15 @@
+const jwt = require("jsonwebtoken");
+
 module.exports = (req, res) => {
-  res.send("Hello World!");
-};
+  if (req.body.password === process.env.PASSWORD) {
+    const token = jwt.sign({
+        userId: 1,
+    }, process.env.SECRET);
+
+    res.json({
+        token,
+    });
+  } else {
+    res.status(401).send("Wrong password");
+  }   
+}
